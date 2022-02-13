1f2e94842f0c77be1b5b5830214a4f3b32644ed9
Kumar Shashank <1711shashank@gmail.com>
2022-02-13 11:55:30 +0530

    before cookie

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { create } = require("./mongoDB");
 const userDataBase = require('./mongoDB');
 
 const app = express();
@@ -10,40 +9,61 @@ const authRouter = express.Router();
 app.use('/', authRouter)
 
 authRouter
-.route('/signup')
-.post(createAccount);
+    .route('/signup')
+    .post(createAccount);
 
 authRouter
-.route('/login')
-.post(loginUser);
+    .route('/login')
+    .post(loginUser);
 
 
 // function getUser(req,res){
 //     res.send(users);
 // }
 
-async function createAccount(req,res){
+async function createAccount(req, res) {
     let dataObj = req.body;
     let user = await userDataBase.create(dataObj);
     res.json({
-        message:"Account created Successfully",
+        message: "Account created Successfully",
         data: user
     });
 }
 
-async function loginUser(req,res){
-    let dataObj = req.body;
-    let user = await userDataBase.findOne({email: dataObj.email});
-    if(user){
-        res.json({
-            message:"LogIn Successfully",
-            data: user
-        });
-    }else{
-        res.json({
-            message:"User does not exist",
-        });
+async function loginUser(req, res) {
+
+    try {
+        let dataObj = req.body;
+        if (dataObj.email) {
+            let user = await userDataBase.findOne({ email: dataObj.email });
+            if (user) {
+                if (user.password == dataObj.password) {
+                    res.json({
+                        message: "LogIn Successfully",
+                        data: user
+                    });
+                }
+                else {
+                    res.json({
+                        message: "Invalid Password",
+                    });
+                }
+            } else {
+                res.json({
+                    message: "User does not exist",
+                });
+            }
+        }
+        else {
+            return res.json({
+                message: 'Wrong credantials'
+            })
+
+        }
     }
-    
+    catch (err) {
+        console.log(err);
+    }
+
 
 }
