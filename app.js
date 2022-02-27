d952c9b1ae99764355ee6351cdb87b78288df894
Kumar Shashank <1711shashank@gmail.com>
2022-02-27 13:29:56 +0530

    deploy

--- app.js
+++ app.js
@@ -6,19 +6,14 @@ const app = express();
 
 app.use(cors({ origin: true, optionsSuccessStatus: 200, credentials: true }));
 app.options( "*", cors({ origin: true, optionsSuccessStatus: 200, credentials: true }) );
-app.use(function(req, res, next) {
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
+
 
 app.use(express.json(),cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.listen(3000);
+
+const port = process.env.PORT || 3000
+
+app.listen(port);
 
 const authRouter = require('./controller/authRouter');
 app.use('/', authRouter);
